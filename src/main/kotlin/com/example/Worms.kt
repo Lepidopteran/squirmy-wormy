c227fb60d39eef56db303bdd8593eac37ea8fd99
dancesaurus <themistakeofsuccess@gmail.com>
2025-07-15 18:15:09 -0400

    feat(earthworm): add texture and item utilities

--- src/main/kotlin/com/example/Worms.kt
+++ src/main/kotlin/com/example/Worms.kt
@@ -5,6 +5,10 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents.ModifyEntries
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.item.FoodComponent
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
 import net.minecraft.registry.Registries
@@ -15,18 +19,24 @@ import org.slf4j.LoggerFactory
 
 object ModItems {
 
-    val EARTH_WORM: Item? =
-        register( // Ignore the food component for now, we'll cover it later in the food section.
-            Item(FabricItemSettings()),
-            "earth_worm"
+    val EARTHWORM: Item? =
+        register(
+            Item(FabricItemSettings()
+                .food(FoodComponent.Builder()
+                .snack()
+                .statusEffect(StatusEffectInstance(
+                StatusEffects.NAUSEA, 6*20, 1), 1.0f)
+                .build())),
+            "earthworm"
         )
 
     fun initialize() {
 
-        // Get the event for modifying entries in the ingredients group.
-// And register an event handler that adds our suspicious item to the ingredients group.
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-            .register(ModifyEntries { itemGroup: FabricItemGroupEntries? -> itemGroup!!.add(EARTH_WORM) })
+        CompostingChanceRegistry.INSTANCE.add(EARTHWORM, 1.0f)
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+            .register(ModifyEntries { itemGroup: FabricItemGroupEntries? -> itemGroup!!.add(EARTHWORM) })
     }
 
     fun register(item: Item, id: String): Item {
