895f1c4b598f8e2db10f39c6cffaacec2789439c
dancesaurus <themistakeofsuccess@gmail.com>
2025-07-18 20:47:17 -0400

    trying to add block still

--- src/main/java/dancesaurus/squirmy_wormy/Blocks/ModBlocks.java
+++ src/main/java/dancesaurus/squirmy_wormy/Blocks/ModBlocks.java
@@ -3,7 +3,9 @@ package dancesaurus.squirmy_wormy.Blocks;
 import dancesaurus.squirmy_wormy.ModInfo;
 import dancesaurus.squirmy_wormy.SquirmyWormy;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -11,6 +13,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+
+    public static final Block GLOW_WORM_WEB = registerBlock(name."glow_worm_web",
+            new Block(FabricBlockSettings.copyOf(Blocks.GLOW_LICHEN)))
 
 
     private static Block registerBlock(String name, Block block) {
@@ -20,7 +25,7 @@ public class ModBlocks {
 
 private static Item registerBlockItem(String name, Block block) {
     return Registry.register(Registries.ITEM, new Identifier(ModInfo.MOD_ID, name),
-            new BlockItem(block, new FabricItemSettings()))
+            new BlockItem(block, new FabricItemSettings()));
 
     }
 }
