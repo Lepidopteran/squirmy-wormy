d21b39754d69c3e839e29dc07dec679a34503327
dancesaurus <themistakeofsuccess@gmail.com>
2025-07-21 14:32:14 -0400

    added ability to generate data

---
+++ src/main/java/dancesaurus/squirmy_wormy/DataGenerator.java
@@ -0,0 +1,11 @@
+package dancesaurus.squirmy_wormy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+    }
+}
