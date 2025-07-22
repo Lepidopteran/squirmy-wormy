f020fc125a33b0486318437a5ec21777187349e0
Blaine <blainenicholls@proton.me>
2025-07-22 15:29:56 -0400

    fix(glow_worm_web): add transparent layer

--- src/client/java/dancesaurus/squirmy_wormy/SquirmyWormyClient.java
+++ src/client/java/dancesaurus/squirmy_wormy/SquirmyWormyClient.java
@@ -1,8 +1,10 @@
 package dancesaurus.squirmy_wormy;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.model.EntityModelLayer;
 import net.minecraft.util.Identifier;
 
@@ -13,6 +15,7 @@ public class SquirmyWormyClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		EntityRendererRegistry.register(SquirmyWormy.EARTHWORM, EarthwormRenderer::new);
 		EntityRendererRegistry.register(SquirmyWormy.GLOW_WORM, GlowWormRenderer::new);
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GLOW_WORM_WEB, RenderLayer.getCutout());	
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
