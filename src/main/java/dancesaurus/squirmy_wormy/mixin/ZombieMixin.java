9e7506ab0148f6ff3f1c38917cabe41a2afe9a60
dancesaurus <themistakeofsuccess@gmail.com>
2025-07-22 14:00:04 -0400

    zombie worm spawn on death added

--- src/main/java/dancesaurus/squirmy_wormy/mixin/ZombieMixin.java
+++ src/main/java/dancesaurus/squirmy_wormy/mixin/ZombieMixin.java
@@ -1,9 +1,12 @@
 package dancesaurus.squirmy_wormy.mixin;
 
+import dancesaurus.squirmy_wormy.SquirmyWormy;
 import dancesaurus.squirmy_wormy.entities.Earthworm;
 import net.minecraft.entity.ai.goal.FleeEntityGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.mob.SlimeEntity;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +17,18 @@ public abstract class ZombieMixin extends PathAwareEntity {
 
 	protected ZombieMixin() {
 		super(null, null);
+	}
+
+	@Override
+	public void remove(RemovalReason reason) {
+		if (this.random.nextFloat() <= 0.15) {
+			Earthworm entity = SquirmyWormy.EARTHWORM.create(this.getWorld());
+			if (entity != null){
+				entity.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.random.nextFloat() * 360.0F, 0.0F);
+				this.getWorld().spawnEntity(entity);
+			}
+		}
+		super.remove(reason);
 	}
 
 	@Inject(at = @At("TAIL"), method = "initGoals")
