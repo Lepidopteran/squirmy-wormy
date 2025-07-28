63d42b9b74aadb5680bcc64963182b6a547f9fef
Blaine <blainenicholls@proton.me>
2025-07-28 17:41:24 -0400

    refactor(glow worm): fix scaling for baby worms

--- src/client/java/dancesaurus/squirmy_wormy/GlowWormRenderer.java
+++ src/client/java/dancesaurus/squirmy_wormy/GlowWormRenderer.java
@@ -2,7 +2,9 @@ package dancesaurus.squirmy_wormy;
 
 import dancesaurus.squirmy_wormy.entities.Earthworm;
 import dancesaurus.squirmy_wormy.entities.GlowWorm;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
@@ -13,5 +15,16 @@ public class GlowWormRenderer extends GeoEntityRenderer<GlowWorm> {
     public GlowWormRenderer(EntityRendererFactory.Context context) {
         super(context, new DefaultedEntityGeoModel<>(new Identifier(ModInfo.MOD_ID, "glow_worm")));
         addRenderLayer(new AutoGlowingGeoLayer<>(this));
+    }
+
+    @Override
+    public void render(GlowWorm entity, float entityYaw, float partialTick, MatrixStack poseStack, VertexConsumerProvider bufferSource, int packedLight) {
+        if(entity.isBaby()) {
+            poseStack.scale(0.5f, 0.5f, 0.5f);
+        } else {
+            poseStack.scale(1f, 1f, 1f);
+        }
+
+        super.render(entity, entityYaw, partialTick, poseStack, bufferSource, packedLight);
     }
 }
