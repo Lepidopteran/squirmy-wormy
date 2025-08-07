5b893e49c6d4d751cd3bfde89cd14450c9a6137b
Blaine <blainenicholls@proton.me>
2025-08-07 19:53:04 -0400

    feat(glow worm web): get connective block states partially working

--- src/main/java/dancesaurus/squirmy_wormy/blocks/GlowWormWeb.java
+++ src/main/java/dancesaurus/squirmy_wormy/blocks/GlowWormWeb.java
@@ -6,30 +6,48 @@ import net.minecraft.block.Blocks;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.StateManager.Builder;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.IntProperty;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+
+import java.util.Locale;
 
 @SuppressWarnings("deprecation")
 
 public class GlowWormWeb extends Block {
 	public GlowWormWeb(Settings settings) {
 		super(settings);
+		setDefaultState(this.getDefaultState().with(SEGMENT, SegmentType.MIDDLE));
+	}
+	public static final EnumProperty<SegmentType> SEGMENT = EnumProperty.of("segment", SegmentType.class);
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(SEGMENT);
 	}
 
-	private void breakChain(ServerWorld world, BlockPos startPos, Block block) {
-		BlockPos currentPos = startPos.down();
-		while (true) {
-			BlockState state = world.getBlockState(currentPos);
-			if (state.isOf(block)) {
-				world.breakBlock(currentPos, true);
-				currentPos = currentPos.down();
-			} else {
-				break;
-			}
+	@Override
+	public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+		updateSegment(world, pos);
+		updateSegment(world, pos.down());
+		updateSegment(world, pos.up());
+	}
+
+	private void updateSegment(World world, BlockPos pos) {
+		BlockState state = world.getBlockState(pos);
+		if (!state.isOf(this)) return;
+
+		BlockState above = world.getBlockState(pos.up());
+		SegmentType segment = above.isOf(this) ? SegmentType.TIP : SegmentType.MIDDLE;
+
+		if (state.get(SEGMENT) != segment) {
+			world.setBlockState(pos, state.with(SEGMENT, segment), Block.NOTIFY_ALL);
 		}
 	}
 
@@ -39,17 +57,10 @@ public class GlowWormWeb extends Block {
 	}
 
 	@Override
-	public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
-		if (!state.canPlaceAt(world, pos)) {
-			breakChain(world, pos, state.getBlock());
-		}
-	}
-	
-	@Override
 	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
 		if (direction == Direction.UP && !state.canPlaceAt(world, pos)) {
 			if (!world.isClient()) {
-				world.scheduleBlockTick(pos, state.getBlock(), 100);
+				world.scheduleBlockTick(pos, state.getBlock(), 1);
 			}
 
 			return Blocks.AIR.getDefaultState();
@@ -61,5 +72,14 @@ public class GlowWormWeb extends Block {
 	@Override
 	public StateManager<Block, BlockState> getStateManager() {
 		return super.getStateManager();
+	}
+
+	public enum SegmentType implements StringIdentifiable {
+		TIP, MIDDLE;
+
+		@Override
+		public String asString() {
+			return name().toLowerCase(Locale.ROOT);
+		}
 	}
 }  
