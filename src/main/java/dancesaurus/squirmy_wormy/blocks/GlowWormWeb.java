ab2ba3cc789872c5b8bb4914b801076cffc6d7db
Blaine <blainenicholls@proton.me>
2025-08-07 23:04:36 -0400

    refactor(glow worm web): break chains gradually

--- src/main/java/dancesaurus/squirmy_wormy/blocks/GlowWormWeb.java
+++ src/main/java/dancesaurus/squirmy_wormy/blocks/GlowWormWeb.java
@@ -1,18 +1,17 @@
 package dancesaurus.squirmy_wormy.blocks;
 
-import dancesaurus.squirmy_wormy.ModInfo;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
-import org.slf4j.LoggerFactory;
 
 import java.util.Locale;
 
@@ -49,11 +48,21 @@ public class GlowWormWeb extends Block {
 
 	@Override
 	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
-		if (direction == Direction.UP && !state.canPlaceAt(world, pos)) {
-			return Blocks.AIR.getDefaultState();
+		if (direction == Direction.UP && !state.canPlaceAt(world, pos) && !world.isClient()) {
+			world.scheduleBlockTick(pos, this, 1);
 		}
 
 		return state;
+	}
+
+	@Override
+	public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		world.breakBlock(pos, true);
+
+		BlockPos below = pos.down();
+		if (world.getBlockState(below).isOf(this)) {
+			world.scheduleBlockTick(below, this, 1);
+		}
 	}
 
 	@Override
